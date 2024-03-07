3b129f6d89e0d2befca202507f4076cbbb04b44c
Abhishek kumar <121367766+PythonboyAB@users.noreply.github.com>
2024-03-07 19:55:10 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,21 +1,53 @@
-const startBtn= document.querySelector(".start-btn");
+const startBtn = document.querySelector(".start-btn");
 const popupInfo = document.querySelector(".popup-info");
-const exitBtn=document.querySelector(".exit-btn");
-const main=document.querySelector(".main");
-const continueBtn=document.querySelector(".contiune-btn");
-const quizSection=document.querySelector(".quiz-section");
+const exitBtn = document.querySelector(".exit-btn");
+const main = document.querySelector(".main");
+const continueBtn = document.querySelector(".continue-btn");
+const quizSection = document.querySelector(".quiz-section");
+const quizBox = document.querySelector(".quiz-box");
 
-
-startBtn.onclick =() =>{
-    popupInfo.classList.add('active');
-    main.classList.add('active');
+startBtn.onclick = () => {
+  popupInfo.classList.add("active");
+  main.classList.add("active");
 }
-exitBtn.onclick =() =>{
-   popupInfo.classList.remove('active');
-   main.classList.remove('active');
+exitBtn.onclick = () => {
+  popupInfo.classList.remove("active");
+  main.classList.remove("active");
+}
+
+continueBtn.onclick = () => {
+  quizSection.classList.add("active");
+  popupInfo.classList.remove("active");
+  main.classList.remove("active");
+  quizBox.classList.add('active');
+  showQuestion(0);
 }
 
 
-continueBtn.onClick = ()=>{
-    que
+let quetionCount=0;
+const nextbtn=document.querySelector(".next-btn")
+nextbtn.onclick=()=>{
+  quetionCount++;
+  showQuestion(quetionCount);
+}
+
+
+
+
+
+
+
+
+
+// getting the question from the array
+
+function showQuestion(index){
+  const questionText= document.querySelector(".question-text");
+  questionText.textContent=`${questions[index].numb}. ${questions[index].question}`;
+
+  let optionTag=`<div class="option"><span>${questions[index].options[0]}</span></div>
+  <div class="option"><span>${questions[index].options[1]}</span></div>
+  <div class="option"><span>${questions[index].options[2]}</span></div>
+  <div class="option"><span>${questions[index].options[3]}</span></div>`;
+  optionList.innerHtml= optionTag;
 }
